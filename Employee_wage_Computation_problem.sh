c28ea7fee100d933ab07ad73cb1486d9c8efafe4
Farzeen <farzeenkhan1412@gmail.com>
2021-07-10 11:45:52 +0530

    wage

--- Employee_wage_Computation_problem.sh
+++ Employee_wage_Computation_problem.sh
@@ -1,7 +1,12 @@
 ispresent=0
 isabsent=1
+wageperhr=20
 num=$((RANDOM%2))
 case $num in
-	0) echo "Employee is present";;
-	1) echo "Employee is absent";;
+	0) workinghr=8
+	   echo "Employee is present";;
+	1) workinghr=0
+	   echo "Employee is absent";;
 esac
+wage=$((workinghr*wageperhr))
+echo "wage is " $wage
