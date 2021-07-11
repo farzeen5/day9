c1cbac2a8521b0e545b0a75f2c089e3a5a2a55fc
Farzeen <farzeenkhan1412@gmail.com>
2021-07-11 19:14:36 +0530

    Part time

--- Employee_wage_Computation_problem.sh
+++ Employee_wage_Computation_problem.sh
@@ -1,12 +1,15 @@
 ispresent=0
 isabsent=1
+isparttime=2
 wageperhr=20
-num=$((RANDOM%2))
+num=$((RANDOM%3))
 case $num in
 	0) workinghr=8
 	   echo "Employee is present";;
 	1) workinghr=0
 	   echo "Employee is absent";;
+	2)workinghr=4
+	   echo "Employee is part time"
 esac
 wage=$((workinghr*wageperhr))
 echo "wage is " $wage
