d34e52f8d061a043e09c3f0e293559f8b8cd7588
Farzeen <farzeenkhan1412@gmail.com>
2021-07-13 10:47:50 +0530

    array

--- Employee_wage_Computation_problem.sh
+++ Employee_wage_Computation_problem.sh
@@ -1,16 +1,19 @@
+ispresent=0
+isparttime=2
+isabsent=1
 wageperhr=20
-function workinghr()
-{
-case $1 in
+for ((i=0;i<20:i++))
+do
+num=$((RANDOM%3))
+case $num in
 	0) workinghr=8 ;;
 	1) workinghr=0 ;;
 	2) workinghr=4 ;;
 esac 
-	echo $workinghr
-}
-workinghr="$(workinghr $((RANDOM%3)))"
-wage=$(((wageperhr*workinghr)))
-echo "workinghr is " $workinghr
-echo "total month wage is " $wage
+	salary=$((salary+(wageperhr*workinghr)))
+	array[i]=$((wageperhr*workinghr))
+done
+echo ${array[@]}
+echo "total month wage" $salary
 
 
