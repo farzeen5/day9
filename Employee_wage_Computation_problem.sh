3ece8f39a824fab6629198944cc550e5b6474b3a
Farzeen <farzeenkhan1412@gmail.com>
2021-07-13 09:28:18 +0530

    function

--- Employee_wage_Computation_problem.sh
+++ Employee_wage_Computation_problem.sh
@@ -1,23 +1,16 @@
-ispresent=0
-isabsent=1
-isparttime=2
 wageperhr=20
-workinghr=0
-workingday=0
-while [ $workinghr -lt 100 ] && [ $workingday -lt 20 ]
-do
-for((i=0;i<20;i++))
-do
-num=$((RANDOM%3))
-case $num in
-	0) workinghr=8
-		workingday=$((workingday+1));;
-	1) workinghr=0
-		workingday=$((workingday+1));;
-	2) workinghr=4
-		 workingday=$((workingday+1));;
-esac
-	wage=$((wage+(workinghr*wageperhr)))
-done
-done
+function workinghr()
+{
+case $1 in
+	0) workinghr=8 ;;
+	1) workinghr=0 ;;
+	2) workinghr=4 ;;
+esac 
+	echo $workinghr
+}
+workinghr="$(workinghr $((RANDOM%3)))"
+wage=$(((wageperhr*workinghr)))
+echo "workinghr is " $workinghr
 echo "total month wage is " $wage
+
+
